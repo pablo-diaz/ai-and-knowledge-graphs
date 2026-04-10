163e1a496426ec8f45fbfa1f1cd66a6ae2e3ee20
Pablo Andrés Díaz <pabloandresd@gmail.com>
2026-04-10 15:50:59 -0500

    Add files via upload

---
+++ ConsoleWithAgentFramework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Common;
+using Infrastructure;
+using Application.WorkflowOrchestrationLogic;
+
+namespace ConsoleWithAgentFramework;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var geminiApiKey = UserInputViaKeyboard.GetApiKey(fromProvider: Constants.LlmProvider.Gemini);
+
+        using var neo4jService = new Neo4jService(url: "neo4j://ubuntu-server-01.mshome.net:7687");
+
+        using var fullLlm = new DecoratorForChatClient(provider: Constants.LlmProvider.Gemini,
+            llmApiKey: geminiApiKey, modelName: Constants.LlmModels.Gemini.FullModel);
+
+        using var lightLlm = new DecoratorForChatClient(provider: Constants.LlmProvider.Gemini,
+            llmApiKey: geminiApiKey, modelName: Constants.LlmModels.Gemini.LightModel);
+
+        await Logic.ProcessEachUserQuestion(
+            userQuestionsToProcess: Utilities.GetQuestionsFromUser(
+                maybeUserQuestionProvided: args.Length == 1 ? args[0] : null),
+            settings: new(FullLlm: fullLlm, LightLlm: lightLlm, neo4jService),
+            fnReportProgress: progress => {
+                Console.WriteLine(progress);
+                return Task.CompletedTask;
+            });
+    }
+
+}
