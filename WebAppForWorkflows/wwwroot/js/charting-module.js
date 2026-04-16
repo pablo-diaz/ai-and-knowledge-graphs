31b609f11db7a2bc8d64a55dde4370423974c133
Pablo Andrés Díaz <pabloandresd@gmail.com>
2026-04-16 11:40:30 -0500

    Add files via upload

---
+++ WebAppForWorkflows/wwwroot/js/charting-module.js
@@ -0,0 +1,31 @@
+﻿import mermaid from 'https://unpkg.com/mermaid@11.14.0/dist/mermaid.esm.mjs';
+
+let chartsCounter = 1;
+
+async function renderMermaid(text) {
+	try {
+		chartsCounter++;
+		const { svg } = await mermaid.render(`graphDiv${chartsCounter}`, text);
+		return svg;
+	} catch (err) {
+		console.error("Render failed:", err);
+		return `<pre style="color:red;">Chart could not be rendered.<br/><br/>${text}</pre>`;
+	}
+}
+
+export async function adjustAllMermaidJsCharts(withinElement) {
+	mermaid.initialize({
+		startOnLoad: false,
+		theme: 'forest',
+		parseError: (err, hash) => {
+			console.error('Mermaid syntax error:', err);
+		}
+	});
+
+	const mermaidJsElements = withinElement.querySelectorAll('code.language-mermaid');
+	for (const el of mermaidJsElements) {
+		const newChart = document.createElement('div');
+		newChart.innerHTML = await renderMermaid(el.textContent.trim());
+		el.replaceWith(newChart);
+	}
+}
--- WebAppForWorkflows/wwwroot/js/eventHandling.js
+++ WebAppForWorkflows/wwwroot/js/eventHandling.js
@@ -1,18 +1,51 @@
-﻿const sessionId = document.getElementById('txtSessionId').textContent;
+﻿let contentCounter = 0;
+let isReceivingMessages = false;
+
+const infoContainer = document.getElementById('txtInformation');
+
+const cancelButton = document.getElementById('btnCancel');
+cancelButton.addEventListener('click', e => {
+    if (isReceivingMessages) {
+        es.close();
+        isReceivingMessages = false;
+        cancelButton.style.display = 'none';
+        infoContainer.textContent = "Cancelled";
+    }
+    e.preventDefault();
+});
+
+const sessionId = document.getElementById('txtSessionId').textContent;
 const es = new EventSource(`/events?sid=${sessionId}`);
-const eventContainer = document.getElementById('dvProgress');
-const emptyState = document.getElementById('empty-state');
 
-let contentCounter = 0;
+es.onopen = () => {
+    cancelButton.style.display = 'block';
+    isReceivingMessages = true;
+};
 
-es.onmessage = (e) => console.log("Generic message: ", e.data);
+es.onmessage = (e) => console.warn(`Unhandled message of type '${e.event}' with message content: '${e.data}'`);
+
 es.onerror = () => console.log('Reconnecting...');
 
 es.addEventListener('finished', e => {
     es.close();
+    infoContainer.textContent = "Finished";
+    cancelButton.style.display = 'none';
 });
 
+es.addEventListener('errorsReport', e => console.error(e.data));
+
+es.addEventListener('warningsReport', e => console.warn(e.data));
+
+es.addEventListener('queryCreated', e => console.info(`Query to execute: ${e.data}`));
+
 es.addEventListener('progressReport', e => {
+    const message = JSON.parse(e.data);
+    console.info(message.content);
+    infoContainer.textContent = message.content;
+});
+
+es.addEventListener('userResponse', e => {
+    const emptyState = document.getElementById('txt-to-remove');
     if (emptyState) emptyState.remove();
 
     const message = JSON.parse(e.data);
@@ -33,11 +66,18 @@ es.addEventListener('progressReport', e => {
         </div>
     `;
 
+    const eventContainer = document.getElementById('dvFinalAnswer');
     eventContainer.insertAdjacentHTML('beforeend', newContentHtml);
 
-    const newTables = document.querySelectorAll(`#dvProgress #${contentId} table`);
+    const newTables = document.querySelectorAll(`#dvFinalAnswer #${contentId} table`);
     newTables.forEach(table => {
         table.classList.add('table', 'table-bordered', 'table-hover', 'mt-3');
     });
+
+    import('./charting-module.js')
+    .then((chartingModule) => {
+        chartingModule.adjustAllMermaidJsCharts(document.querySelector(`#dvFinalAnswer #${contentId}`));
+    })
+    .catch((err) => console.error('Error loading charting module:', err));
 
 });
